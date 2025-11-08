27ade23093a9e1bacac52b48a29c97af6df94e86
Maxime BELLET <pdmtc.bellet@gmail.com>
2025-11-08 12:25:01 +0100

    fix: no header on sponsors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,11 @@ export const routes: Routes = [{
       component: MainLayout,
       children: [
         { path: '', loadComponent: () => import('./pages/home').then(m => m.Home) },
+        {
+          path: 'sponsors',
+          title: 'Sponsors', loadComponent: () => import('./pages/sponsors/sponsors').then(m => m.SponsorComponent)
+        },
       ],
-    },
-    {
-      path: 'sponsors',
-      title: 'Sponsors', loadComponent: () => import('./pages/sponsors/sponsors').then(m => m.SponsorComponent)
     },
     {
       path: '404',
