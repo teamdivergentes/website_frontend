f7616c5bb3b5489c5f1c548d244044fcd140f081
Solène Murcia <35804493+Stytti@users.noreply.github.com>
2025-11-17 21:39:53 +0100

    [Layout] Footer

--- src/shared/layouts/main-layout/main-layout.ts
+++ src/shared/layouts/main-layout/main-layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Header} from '../../headers/header/header';
+import {Footer} from '../footer/footer';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     RouterOutlet,
     Header,
+    Footer
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
