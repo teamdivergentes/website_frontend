e0706294f3ac412233777c51492cdae9044fc298
Maxime BELLET <pdmtc.bellet@gmail.com>
2026-01-17 11:41:33 +0100

    fix(tailwind): Modification et corrections des styles + ajout commentaire

--- src/app/components/font-test/font-test.ts
+++ src/app/components/font-test/font-test.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './font-test.html',
-  styleUrl: './font-test.scss',
 })
 export class FontTestComponent {
 
