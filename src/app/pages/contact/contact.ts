3ea80c78e88989c7838198c3a58e2b0f7fdca333
Maxime BELLET <pdmtc.bellet@gmail.com>
2026-01-25 14:51:31 +0100

    feat: Revue style contact

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrls: ['./contact.scss']
 })
